1cb2befa7ea87265c5f13b55cefe37f9a6b92c39
Bob Amand <biotmg@nc.rr.com>
2015-09-13 00:47:56 -0400

    draft tests 13-Sep-2015 00:15 am

--- test_movie_lib.py
+++ test_movie_lib.py
@@ -2,31 +2,40 @@
 #import unittest
 from movie_lib import *
 
+user1 = User(666)     #note User(), refers to class User object.
+user2 = User(123)
+user3 = User(99)
+user4 = User(0)
+movie1 = Movie(23, 'The Matrix')    #Two parameterss
+movie2 = Movie(45, 'Toy Story')
+rating1 = Rating('B', 45, 3)        #Three parameters
+rating2 = Rating(123, 45, 5)
+rating3 = Rating(user1.uid, movie2.mid, 1)
+rating4 = Rating(user2.uid, movie2.mid, 5)
+
 
 def test_user_creation():
-    user1 = User('A')     #note User(), refers to class User object.
-    user2 = User(123)
-    user3 = User(99)
-    user4 = User(0)
-    assert user1.uid == 'A'
+    assert user1.uid == 666
     assert user2.uid == 123
     assert user3.uid == 99
     assert user4.uid == 0
 
 def test_movie_creation(): # takes 2 parameters: movie_id, title
-    movie1 = Movie(23, 'The Matrix')
-    movie2 = Movie(45, 'Toy Story')
     assert movie1.mid == 23
     assert movie1.tid == 'The Matrix'
     assert movie2.mid == 45
     assert movie2.tid =='Toy Story'
 
 def test_rating_creation(): # takes 3 parameters: user_id, movie_id, star
-    rating1 = Rating('A', 45, 3)
-    rating2 = Rating(123, 45, 5)
-    assert rating1.uid == 'A'
+    assert rating1.uid == 'B'
     assert rating1.mid == 45
     assert rating1.sid == 3
     assert rating2.uid == 123
     assert rating2.mid == 45
     assert rating2.sid == 5
+    assert rating3.uid == 666
+    assert rating3.mid == 45
+    assert rating3.sid == 1
+    assert rating4.uid == 123
+    assert rating4.mid == 45
+    assert rating4.sid == 5
