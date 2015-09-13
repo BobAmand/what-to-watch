e66abf9a210c55ebd5dc94094960a32a3ba66f1e
Bob Amand <biotmg@nc.rr.com>
2015-09-13 11:52:15 -0400

    rating tests passed finally

--- movie_lib.py
+++ movie_lib.py
@@ -1,17 +1,40 @@
-# movie_lib.py
-# This will contain my classes
-#
+
+all_movies = {}
+all_users = {}
+
 class User:
     def __init__(self, user_id):
         self.uid = user_id
+        all_users[self.uid] = self
 
 class Movie:
     def __init__(self, movie_id, title):
         self.mid = movie_id
         self.tid = title
+        all_movies[self.mid] = self
+        self.ratings = {} #key is user_id, value: Rating object
+
+    def add_rating(self, rating):
+        self.ratings[rating.uid] = rating   #user_id is key
+
+    def get_ratings(self):
+        return self.ratings.values()
+
+    def __str__(self):          # a string representation of Movie() class
+        return 'Movie(movie_id={}, title={})'. format(self.mid, repr(self.tid))
+
+    def __repr__(self):         # brings the string representation into dict
+        return self.__str__()
 
 class Rating:
     def __init__(self, user_id, movie_id, star):
         self.uid = user_id
         self.mid = movie_id
         self.sid = star
+        all_movies[self.mid].add_rating(self)
+
+    def __str__(self):
+        return 'Rating(user_id={}, movie_id={}, stars={})'. format(self.uid, self.mid, self.sid)
+
+    def __repr__(self):
+        return self.__str__()
--- test_movie_lib.py
+++ test_movie_lib.py
@@ -1,22 +1,30 @@
 # test script - What to Watch
-#import unittest
+# import unittest
 from movie_lib import *
 
-user1 = User(666)     #note User(), refers to class User object.
-user2 = User(123)
+user1 = User(123)     #note User(), refers to class User object.
+user2 = User(456)
 user3 = User(99)
 user4 = User(0)
 movie1 = Movie(23, 'The Matrix')    #Two parameterss
 movie2 = Movie(45, 'Toy Story')
-rating1 = Rating('B', 45, 3)        #Three parameters
-rating2 = Rating(123, 45, 5)
+rating1 = Rating(123, 23, 3)        #Three parameters
+rating2 = Rating(456, 23, 5)
 rating3 = Rating(user1.uid, movie2.mid, 1)
 rating4 = Rating(user2.uid, movie2.mid, 5)
 
+print(all_movies)   # passes all movies into dictionary
+
+print(all_movies[45])   # passes one movie into the dictionary
+print(all_movies[23])
+
+print(id(movie2))   #'the id() function reveals memory location'
+print(id(all_movies[45]))  # passes in all_movies[self.mid]
+                    # The above will show the same memory location!
 
 def test_user_creation():
-    assert user1.uid == 666
-    assert user2.uid == 123
+    assert user1.uid == 123
+    assert user2.uid == 456
     assert user3.uid == 99
     assert user4.uid == 0
 
@@ -27,15 +35,24 @@ def test_movie_creation(): # takes 2 parameters: movie_id, title
     assert movie2.tid =='Toy Story'
 
 def test_rating_creation(): # takes 3 parameters: user_id, movie_id, star
-    assert rating1.uid == 'B'
-    assert rating1.mid == 45
+    assert rating1.uid == 123
+    assert rating1.mid == 23
     assert rating1.sid == 3
-    assert rating2.uid == 123
-    assert rating2.mid == 45
+    assert rating2.uid == 456
+    assert rating2.mid == 23
     assert rating2.sid == 5
-    assert rating3.uid == 666
+    assert rating3.uid == 123
     assert rating3.mid == 45
     assert rating3.sid == 1
-    assert rating4.uid == 123
+    assert rating4.uid == 456
     assert rating4.mid == 45
     assert rating4.sid == 5
+
+def test_find_ratings_for_movie():
+    #toy_story_ratings = get_ratings_for_movie(movie2.mid)
+        # alternative approaches
+    # Return a list of Rating objects
+    toy_story_ratings = all_movies[movie2.mid].get_ratings()
+    print(len(toy_story_ratings))
+    print(toy_story_ratings)
+    assert len(toy_story_ratings) == 2   #Two users rated movieToystory
