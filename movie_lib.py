9c2ea22215c6113276a9a384274949c069ba5971
Bob Amand <biotmg@nc.rr.com>
2016-01-29 16:47:48 -0500

    version 02

--- movie_lib.py
+++ movie_lib.py
@@ -47,83 +47,90 @@ class User:
         # self.occ = occupation
         # self.tms = timestamp
         all_users[self.uid] = self  # dict with key = user_id
-        self.ratings = {}
+        # self.ratings = {}
 
-    def get_user_data():
-        with open('u.user', encoding='latin_1') as f:
-            user_reader = csv.DictReader(f, fieldnames=['user_id',
-                                            'age',
-                                            'gender',
-                                            'occupation',
-                                            'zipcode',
-                                            ]
-                                            ,delimiter='|')
-        return user_reader
+    def all_ratings(self):
+        return all_users[self.id]
 
-    def add_rating(self, rating):   #loads the user_id and rating to rating
-        self.ratings[rating.uid] = rating       # duplicate from Movie yet operates locally
-
-    def get_ratings(self):          #gets the ratings by user_id
-        return self.ratings.values()
-
-    def get_ave_rating(self):       # gets average rating by user_id
-        return sum(get_ratings(self))/len(get_ratings(self))
-
-    def __str__(self):
-        return 'User(user_id={})'. format(self.uid)
-
-    def __repr__(self):
-        return self.__str__()
+    # def get_user_data():
+    #     with open('u.user', encoding='latin_1') as f:
+    #         user_reader = csv.DictReader(f, fieldnames=['user_id',
+    #                                         'age',
+    #                                         'gender',
+    #                                         'occupation',
+    #                                         'zipcode',
+    #                                         ]
+    #                                         ,delimiter='|')
+    #     return user_reader
+    #
+    # def add_rating(self, rating):   #loads the user_id and rating to rating
+    #     self.ratings[rating.uid] = rating       # duplicate from Movie yet operates locally
+    #
+    # def get_ratings(self):          #gets the ratings by user_id
+    #     return self.ratings.values()
+    #
+    # def get_ave_rating(self):       # gets average rating by user_id
+    #     return sum(get_ratings(self))/len(get_ratings(self))
+    #
+    # def __str__(self):
+    #     return 'User(user_id={})'. format(self.uid)
+    #
+    # def __repr__(self):
+    #     return self.__str__()
 
 class Movie:
-    def __init__(self, movie_id, title):
-        self.mid = movie_id
-        self.tid = title
-        all_movies[self.mid] = self # dict with key=movie_id, value=title
-        self.ratings = {} #key is user_id, value: Rating object = rating dictionary
-
-    def get_movie_data():
-        with open('u.item', encoding='latin_1') as f:
-            item_reader = csv.DictReader(f, fieldnames=['m_id','m_title'],delimiter='|')
-        return item_reader
-
-    def add_rating(self, rating):   # loads the user_id and rating to ratings dict
-        self.ratings[rating.uid] = rating   #user_id is key
-
-    def get_ratings(self):          # gets the ratings by user_id
-        return self.ratings.values()
-
-    def get_ave_rating(self):       # gets average rating by movie
-        return sum(get_ratings(self))/len(get_rating(self))
+    def __init__(self, movie_id, title=None, ratings=None):
+        self.id = movie_id
+        self.title = all_movies[self.id]
+        self.ratings = all_ratings[self.id]
+        all_movies[self.id] = self # dict with key=movie_id, value=title
+        all_ratings[self.id] = self # key is user_id, value: Rating object = rating dictionary
+    #
+    # def get_movie_data():
+    #     with open('u.item', encoding='latin_1') as f:
+    #         item_reader = csv.DictReader(f, fieldnames=['m_id','m_title'],delimiter='|')
+    #     return item_reader
+    #
+    # def add_rating(self, rating):   # loads the user_id and rating to ratings dict
+    #     self.ratings[rating.uid] = rating   #user_id is key
+    #
+    # def get_ratings(self):          # gets the ratings by user_id
+    #     return self.ratings.values()
+    #
+    # def get_ave_rating(self):       # gets average rating by movie
+    #     return sum(get_ratings(self))/len(get_rating(self))
 
     def __str__(self):          # a string representation of Movie() class
-        return 'Movie(movie_id={}, title={})'. format(self.mid, repr(self.tid))
+        return 'Movie: movie_id = {}, title = {}, ratings = {})'. format(self.id, repr(self.title), repr(self.ratings))
 
     def __repr__(self):         # brings the string representation into dict
         return self.__str__()
 
 class Rating:
-    def __init__(self, user_id, movie_id, star):
-        self.uid = user_id
-        self.mid = movie_id
-        self.sid = star
-        all_movies[self.mid].add_rating(self)
-        all_users[self.uid].add_rating(self)
-
-    def get_rating_data():
-        with open('u.data', encoding='latin_1') as f:
-            data_reader = csv.DictReader(f, fieldnames=['user_id',
-                                            'movie_id',
-                                            'rating',
-                                            ]
-                                            ,delimiter='\t')
-        return data_reader
+    def __init__(self, user_id, movie_id, stars):
+        self.user = user_id
+        self.movie = movie_id
+        self.stars = stars
+    #
+    #     all_movies[self.mid].add_rating(self)
+    #     all_users[self.uid].add_rating(self)
+    #
+    # def get_rating_data():
+    #     with open('u.data', encoding='latin_1') as f:
+    #         data_reader = csv.DictReader(f, fieldnames=['user_id',
+    #                                         'movie_id',
+    #                                         'rating',
+    #                                         ]
+    #                                         ,delimiter='\t')
+    #     return data_reader
 
     def __str__(self):
-        return 'Rating(user_id={}, movie_id={}, stars={})'. format(self.uid, self.mid, self.sid)
+        return 'Rating(user_id={}, movie_id={}, stars={})'. format(self.user, self.movie, self.stars)
 
     def __repr__(self):
         return self.__str__()
 
-def rating_for_movie():
-    movie1 = input("What movie would you like to see ratings?: ")
+    def avg_rating(self):
+        avg = round(sum(self.ratings)/len(self.ratings), 2)
+        return avg
+    # movie1 = input("What movie would you like to see ratings?: ")
